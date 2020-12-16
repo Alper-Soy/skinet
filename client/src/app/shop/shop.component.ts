d51fea0b124b32addc659b92e4b0d69409cc7797
Alper Soy <51034169+Alper-Soy@users.noreply.github.com>
2020-12-16 12:41:59 +0300

    Designing the shop page

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -1,15 +1,26 @@
+import { error } from '@angular/compiler/src/util';
 import { Component, OnInit } from '@angular/core';
+import { IProduct } from '../shared/models/product';
+import { ShopService } from './shop.service';
 
 @Component({
   selector: 'app-shop',
   templateUrl: './shop.component.html',
-  styleUrls: ['./shop.component.scss']
+  styleUrls: ['./shop.component.scss'],
 })
 export class ShopComponent implements OnInit {
+  products: IProduct[];
 
-  constructor() { }
+  constructor(private shopService: ShopService) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.shopService.getProducts().subscribe(
+      (response) => {
+        this.products = response.data;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
-
 }
